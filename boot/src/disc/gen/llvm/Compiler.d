256f3a8ed7d85a9ee9827f615c1cbe601d1adfa8
okard <no@email.org>
2010-10-27 09:12:28 +0200

    start llvm backend

---
+++ boot/src/disc/gen/llvm/Compiler.d
@@ -0,0 +1,37 @@
+/******************************************************************************
+*    Dis Programming Language 
+*    disc - Bootstrap Dis Compiler
+*
+*    disc is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    disc is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with disc.  If not, see <http://www.gnu.org/licenses/>.
+*
+******************************************************************************/
+module disc.gen.llvm.Compiler;
+
+import disc.ast.Node;
+import disc.gen.llvm.LLVM;
+
+
+/**
+* LLVM based Dis Compiler
+*/
+class Compiler
+{
+
+    /**
+    * Compile AST
+    */
+    public void compile(Node ast)
+    {
+    }
+} 
---
+++ boot/src/disc/gen/llvm/LLVM.d
@@ -0,0 +1,193 @@
+/******************************************************************************
+*    Dis Programming Language 
+*    disc - Bootstrap Dis Compiler
+*
+*    disc is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    disc is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with disc.  If not, see <http://www.gnu.org/licenses/>.
+*
+******************************************************************************/
+module disc.gen.llvm.LLVM;
+// This Module wraps LLVM C API back to Classes
+
+import llvm.c.Core;
+
+
+/**
+* LLVM Context
+*/
+class Context
+{
+    // Global Context Instance
+    private static Context mGlobal;
+    // LLVM Context 
+    private LLVMContextRef mContext;
+
+    /**
+    * Create new Context
+    */
+    public this()
+    {
+        mContext = LLVMContextCreate();
+    }
+
+    /**
+    * Create new Context
+    */
+    private this(LLVMContextRef context)
+    {   
+        mContext = context;
+    }
+    
+    /**
+    * Dtor
+    */
+    private ~this()
+    {
+        LLVMContextDispose(mContext);
+    }
+        
+    /**
+    * Static Ctor
+    */
+    public static this()
+    {
+        mGlobal = new Context(LLVMGetGlobalContext());
+    }
+
+    /**
+    * Get the llvmContext
+    */
+    public LLVMContextRef llvmContext()
+    {
+        return mContext;
+    }
+
+    /**
+    * Global Context
+    */
+    public static Context Global()
+    {
+        return mGlobal;
+    }
+} 
+
+/**
+* LLVM Module
+*/
+class Module
+{
+    //LLVM Module
+    private LLVMModuleRef mModule;
+
+    /**
+    * Create new Module
+    */
+    public this(Context context, string name)
+    {
+        mModule = LLVMModuleCreateWithNameInContext((cast(char[])name).ptr, context.llvmContext());
+    }
+
+    /**
+    * Destructor
+    */
+    public ~this()
+    {
+        LLVMDisposeModule(mModule);
+    }
+
+    /**
+    * dump
+    */
+    public void dump()
+    {
+        LLVMDumpModule(mModule);
+    }
+}
+
+/**
+* LLVM Type
+*/
+class Type
+{
+    // LLVM Type
+    private LLVMTypeRef mType;
+
+    /**
+    * Create type from type ref
+    */
+    private this(LLVMTypeRef type)
+    {
+        mType = type;
+    }
+}
+
+/**
+* LLVM Value
+*/
+class Value
+{
+    //LLVM Value
+    private LLVMValueRef mValue;
+
+
+    /**
+    * Get Type
+    */
+    public Type TypeOf()
+    {
+        return new Type(llvmTypeOf());
+    }
+
+    /**
+    * return LLVM Type
+    */
+    public LLVMTypeRef llvmTypeOf()
+    {
+        return  LLVMTypeOf(mValue);
+    }
+}
+
+/**
+* Basic Block
+*/
+class BasicBlock
+{
+    //LLVM Basic Block
+    LLVMBasicBlockRef mBasicBlock;
+}
+
+/**
+* LLVM Builder
+*/
+class Builder
+{
+    //LLVM Builder
+    private LLVMBuilderRef mBuilder;
+
+    /**
+    * Create new Builder
+    */
+    public this(Context context)
+    {
+        mBuilder = LLVMCreateBuilderInContext(context.llvmContext());
+    }
+
+    /**
+    * Destructor
+    */
+    public ~this()
+    {
+        LLVMDisposeBuilder(mBuilder);
+    }
+
+}
