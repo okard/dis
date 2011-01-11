42c31459124026d2e3afefcc9bbaccf1b7b5d4b1
okard <x@x.com>
2011-01-11 20:23:20 +0100

    extend disc wrapper script

--- boot/disc.sh
+++ boot/disc.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-LD_LIBRARY_PATH=./lib/linux32/:$LD_LIBRARY_PATH
+#64 or 32 bit linux
+osbit=$(getconf LONG_BIT)
+
+# if 64bit os add 32 bit lib path
+if [[ "$osbit" == "64" ]]; then
+	LD_LIBRARY_PATH=./lib/linux32/:$LD_LIBRARY_PATH
+fi
 
 ./bin/disc $@
 #gdb --args ./bin/disc $@
