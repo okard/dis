1c45092eece289f14b8bef6eb228fccc0b18b229
okard <d4rkdragon@gmail.com>
2012-03-13 07:13:08 +0100

    fix build for windows

--- boot-d/make.ps1
+++ boot-d/make.ps1
@@ -2,5 +2,5 @@
 
 $files=get-childitem src/ -include *.d -rec | ? { !$_.FullName.Contains("llvm") } | Foreach-Object { $_.FullName}
 
-dmd -w -debug -gc -Isrc -unittest "-od.obj" -ofbin\disc $files
+dmd -w -debug -gc -unittest -Isrc "-Jsrc\dlf\gen\c\" "-od.obj" "-ofbin\disc" $files
     
